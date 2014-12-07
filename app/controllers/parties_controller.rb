a64e0306b93d9e2baf18ea32e0f2a1310bc600eb
amezianeallioui <ameziane.allioui@gmail.com>
2014-12-07 14:58:52 +0100

    Controler party,  single view party, config windows

---
+++ app/controllers/parties_controller.rb
@@ -0,0 +1,10 @@
+class PartiesController < ApplicationController
+  def index
+  	@parties = Party.all
+  end
+
+  def show
+  	@party = Party.find(params[:id])
+  	render status: 404 unless @party != nil
+  end
+end
