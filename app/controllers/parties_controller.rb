86cec5fbd79926b204fe7d1b48f05c3075de2a49
amezianeallioui <ameziane.allioui@gmail.com>
2014-12-07 20:54:00 +0100

    Index all parties&my parties, single view party
    • Page d'accueil : affichage de toutes les soirées + les soirées
    auxquelles est inscrit le user
    • Page d'une soirée : affichage de ses informations

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,13 +1,43 @@
 class PartiesController < ApplicationController
   def index
   	@parties = Party.all
+
+  	@myParties = Array.new
+  	userParties = PartyUser.where(user_id: 4)
+    
+    number=0
+
+  	userParties.each do |p|
+  		@myParties[number] = Party.find(p.party_id)
+  		number += 1
+  	end
   end
 
   def show
   	@party = Party.find(params[:id])
+
+  	# @users = Array.new
+
+  	# partyUsers = PartyUser.where(party_id: @party.id)
+
+   #  number=0
+
+  	# partyUsers.each do |u|
+  	# 	@users[number] = User.find(u.user_id)
+  	# 	number += 1
+  	# end
+
   	render status: 404 unless @party != nil
   end 
-  
+
+  def suscribe
+  	# PartyUser.create(user_id: 1, party_id: params[:id])
+  end
+
+  def unsuscribe 
+
+  end
+
   def new
   end
 end
