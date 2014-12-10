d5aa4bd06926d5b00c93d42a2ae43dd917874f71
emoc11 <emoc11@free.fr>
2014-12-10 16:01:50 +0100

    users controller's test DONE

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,32 +4,32 @@ RSpec.describe UsersController, :type => :controller do
 
   render_views
 
-  describe "GET index" do
-
-    context 'must be logged in to access :' do
-      it "/users/" do
-        get :index
-        expect(response).to have_http_status(302)
-      end
-
-      it "/users/id" do
-          user = User.create(login: "emoc11", email: "emoc11@free.fr", password: "rubyruby", password_confirmation: "rubyruby")
-          get :show, :id => user.id
-          expect(response).to have_http_status(302)
-      end
+  context "Not logged in, can't access :" do
+    it "/users/" do
+      get :index
+      expect(response).to have_http_status(302)
     end
 
+    it "/users/id" do
+        user = User.create(login: "emoc11", email: "emoc11@free.fr", password: "rubyruby", password_confirmation: "rubyruby")
+        get :show, :id => user.id
+        expect(response).to have_http_status(302)
+    end
+  end
+
+  context "Logged in, can access :" do
+    before(:each) do
+      @user = User.create(login: "emoc11", email: "emoc11@free.fr", password: "rubyonrails", password_confirmation: "rubyonrails")
+      @user.save!
+      session[:user_id] = @user.id
+    end
     it "/users/ exist with a title" do
-      user = User.create(login: "emoc11", email: "emoc11@free.fr", password: "rubyonrails", password_confirmation: "rubyonrails")
-      user.save!
       get :index
       expect(response).to have_http_status(:success)
       expect(response.body).to include("All users")
     end
 
     it "/users/ displays a list of users" do
-      user = User.create(login: "emoc11", email: "emoc11@free.fr", password: "rubyonrails", password_confirmation: "rubyonrails")
-      user.save!
       user1 = User.create(login: "test", email: "test@free.fr", password: "rubyruby", password_confirmation: "rubyruby")
       user1.save!
       get :index
@@ -39,12 +39,14 @@ RSpec.describe UsersController, :type => :controller do
     end
 
     it "/users/id show information about the user" do
-      user = User.create(login: "emoc11", email: "emoc11@free.fr", password: "rubyruby", password_confirmation: "rubyruby")
-      user.save!
-      get :show, :id => user.id
+      get :show, :id => @user.id
       expect(response).to have_http_status(:success)
       expect(response.body).to include("emoc11")
       expect(response.body).to include("emoc11@free.fr")
+    end
+
+    after(:each) do
+      session[:user_id] = nil
     end
   end
 end
